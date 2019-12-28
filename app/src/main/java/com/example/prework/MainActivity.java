c7bfab88b7563392e551428555c3f05d7e36487a
Regina Bass <reginabassvdl@gmail.com>
2019-12-28 07:17:32 -0500

    required user stories completed

--- app/src/main/java/com/example/prework/MainActivity.java
+++ app/src/main/java/com/example/prework/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.prework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.i("Regina", "Button Clicked");
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.colorPrimary));   //option Enter for the cast import
+            }
+        });
+        /*findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById()
+            }
+        });*/
     }
 }
